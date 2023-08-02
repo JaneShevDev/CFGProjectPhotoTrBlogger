0b47bbabdc9c9842708d3f7eccc744865981529a
JaneShevDev <113856637+JaneShevDev@users.noreply.github.com>
2023-08-02 12:25:16 +0200

    Project PhotoBloger CFG

---
+++ Project PhotoBloger/script.js
@@ -0,0 +1,26 @@
+function myFunctionA() {
+  var x = document.getElementById("hiddenGalFrance");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myFunctionB() {
+  var y = document.getElementById("hiddenGalRome");
+  if (y.style.display === "none") {
+    y.style.display = "block";
+  } else {
+    y.style.display = "none";
+  }
+}
+
+function myFunctionC() {
+  var y = document.getElementById("hiddenGalCanada");
+  if (y.style.display === "none") {
+    y.style.display = "block";
+  } else {
+    y.style.display = "none";
+  }
+}
